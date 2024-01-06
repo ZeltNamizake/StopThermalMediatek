df9e625845d377b2d0a1c3b37615a4450c5bef48
Zelt Namizake <146328983+ZeltNamizake@users.noreply.github.com>
2024-01-06 11:51:25 +0700

    Add files via upload

---
+++ dthermal.sh
@@ -0,0 +1,8 @@
+#!/system/bin/sh
+
+#Disable Thermal
+stop thermal
+stop thermald
+stop thermal_manager
+stop thermalloadalgod
+stop vendor.thermal-hal-2-0.mtk
---
+++ ethermal.sh
@@ -0,0 +1,8 @@
+#!/system/bin/sh
+
+#Enable Thermal
+start thermal
+start thermald
+start thermal_manager
+start thermalloadalgod
+start vendor.thermal-hal-2-0.mtk
